98502be9083d2ce2ccd6a3bfed5955d9c7392356
lev <leofunow@gmail.com>
2022-04-26 12:34:16 +0300

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,106 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ITSalaryDesign';
-  curtab = 0;
-  Viplati = [{
-    fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-    proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-  },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    }, {
-      fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-    },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    }, {
-      fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-    },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    }, {
-      fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-    },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    }, {
-      fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-    },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    }, {
-      fio: "Иванов И. И.", otcheti: "5 000 ₽ ", sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "19 000 ₽"
-    },
-    {
-      fio: "Петров П. П.", otcheti: "3 000 ₽ ", sobitia: "3 000 ₽",
-      proecti: "22 000 ₽", pokupki: "2 000 ₽", summa: "30 000 ₽"
-    },];
-  Viplati1 = [{
-    fio: "Иванов И. И.", otcheti: 0, sobitia: "2 000 ₽",
-    proecti: "12 000 ₽ ", pokupki: "—", summa: "14 000 ₽", sobitiacnt: 1, proecticnt: 1, pokupkicnt: 0
-  },
-    {
-      fio: "Петров П. П.", otcheti: 1, sobitia: "3 000 ₽",
-      proecti: "22 000 ₽ ", pokupki: "2 000 ₽", summa: "27 000 ₽", sobitiacnt: 1, proecticnt: 3, pokupkicnt: 1
-    },
-    {
-      fio: "Иванов И. И.", otcheti: 0, sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "14 000 ₽", sobitiacnt: 1, proecticnt: 1, pokupkicnt: 0
-    },
-    {
-      fio: "Петров П. П.", otcheti: 1, sobitia: "3 000 ₽",
-      proecti: "22 000 ₽ ", pokupki: "2 000 ₽", summa: "27 000 ₽", sobitiacnt: 1, proecticnt: 3, pokupkicnt: 1
-    },
-    {
-      fio: "Иванов И. И.", otcheti: 0, sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "14 000 ₽", sobitiacnt: 1, proecticnt: 1, pokupkicnt: 0
-    },
-    {
-      fio: "Петров П. П.", otcheti: 1, sobitia: "3 000 ₽",
-      proecti: "22 000 ₽ ", pokupki: "2 000 ₽", summa: "27 000 ₽", sobitiacnt: 1, proecticnt: 3, pokupkicnt: 1
-    },
-    {
-      fio: "Иванов И. И.", otcheti: 0, sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "14 000 ₽", sobitiacnt: 1, proecticnt: 1, pokupkicnt: 0
-    },
-    {
-      fio: "Петров П. П.", otcheti: 1, sobitia: "3 000 ₽",
-      proecti: "22 000 ₽ ", pokupki: "2 000 ₽", summa: "27 000 ₽", sobitiacnt: 1, proecticnt: 3, pokupkicnt: 1
-    },
-    {
-      fio: "Иванов И. И.", otcheti: 0, sobitia: "2 000 ₽",
-      proecti: "12 000 ₽ ", pokupki: "—", summa: "14 000 ₽", sobitiacnt: 1, proecticnt: 1, pokupkicnt: 0
-    },
-    {
-      fio: "Петров П. П.", otcheti: 1, sobitia: "3 000 ₽",
-      proecti: "22 000 ₽ ", pokupki: "2 000 ₽", summa: "27 000 ₽", sobitiacnt: 1, proecticnt: 3, pokupkicnt: 1
-    },
-  ];
-
-  getactivetab(x: number) {
-    if (x == this.curtab)
-      return 'active';
-    else
-      return '';
-  }
-
-  changetab(x: number) {
-    this.curtab = x;
+  constructor() {
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { EmployeeComponent } from './employee/employee.component';
+import { MainTableComponent } from './main-table/main-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    MainTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
